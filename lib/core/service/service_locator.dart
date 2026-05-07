676a0b50baa40312b18fd04130e0f7d96087c214
omar8157 <mrm548500@gmail.com>
2026-05-07 17:23:08 +0300

    Add light-4x splash image to the web splash screen assets ✅✅😍

--- lib/core/service/service_locator.dart
+++ lib/core/service/service_locator.dart
@@ -1,8 +1,9 @@
 import 'package:banking_mobile_app/core/database/cache/cache_helper.dart';
 import 'package:get_it/get_it.dart';
 
-final GetIt sl = GetIt.instance();
+final GetIt sl = GetIt.instance;
 
 void initServicelacotor() {
   sl.registerLazySingleton(() => CacheHelper());
+  
 }
